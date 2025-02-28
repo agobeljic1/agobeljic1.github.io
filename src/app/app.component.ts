026a65fd45cda1a093b652b184321b8adcaaa0b7
Adnan Gobeljic <agobeljic1@etf.unsa.ba>
2025-02-28 12:07:45 +0100

    Remove title switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  titles = ['✋', 'ADNAN GOBELJIC', 'PORTFOLIO', 'OPEN 2 WORK', '😇'];
-  currentIndex = 0;
+export class AppComponent implements OnInit {
   isMobile = false;
 
-  titleChangeInterval = null;
-
-  constructor(private titleService: Title) {}
-
   ngOnInit(): void {
-    this.handleTitleChange();
     this.isMobile =
       /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
         navigator.userAgent
       );
-  }
-
-  ngOnDestroy(): void {
-    clearInterval(this.titleChangeInterval);
-  }
-
-  handleTitleChange() {
-    this.titleChangeInterval = setInterval(() => {
-      this.titleService.setTitle(this.titles[this.currentIndex]);
-      this.currentIndex++;
-      if (this.currentIndex == this.titles.length) {
-        this.currentIndex = 0;
-      }
-    }, 1000);
   }
 }
